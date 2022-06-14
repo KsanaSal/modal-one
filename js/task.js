72b63e1109b5ebb438c05cbb74602197ac7303f1
KsanaSal <oasalivon@gmail.com>
2022-06-14 23:23:51 +0300

    Add task 1-1

---
+++ js/task.js
@@ -0,0 +1,10 @@
+// Task 1-1
+// Выведи на экран общее количество яблок и винограда. Разницу яблок и винограда.
+const apples = 47;
+const grapes = 135;
+const total = apples + grapes;
+console.log(total)
+const diff = apples - grapes;
+console.log(Math.abs(diff))
+
+// Task 1-2
